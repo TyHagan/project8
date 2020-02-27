2ae53b3ec5b9fa669228b443e720045a44185efc
kthagan <kthagan@yahoo.com>
2020-02-26 16:47:15 -1000

    Adding missing files Fixed life bug Jump off bottom destroyer Menu button in pause menu Local high score

---
+++ code/test1/Assets/Scripts/Gameplay/MenuManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject MenuButton;
+
+    void Start()
+    {
+        MenuButton.SetActive(false);
+    }
+
+    public void Menu()
+    {
+        MenuButton.SetActive(false);
+        Time.timeScale = 1.0f;
+        Application.LoadLevel("TitlePage");
+    }
+}
---
+++ code/test1/Assets/Scripts/Start/HighScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreManager : MonoBehaviour
+{
+    public Text HighScore;
+    public string LocalHighScoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HighScore.text = "High Score: " + PlayerPrefs.GetInt("HighScore").ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
